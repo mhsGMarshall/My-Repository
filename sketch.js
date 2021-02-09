362bf5349d094b671a5e8d7ebf76e8e5d1fcc215
mhsGMarshall <78747311+mhsGMarshall@users.noreply.github.com>
2021-02-08 21:42:17 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+//Setup the canvas
+function setup() {
+    // Sets the screen to be 640 pixels wide and 480 pixels high
+    createCanvas(640,480);
+}
+
+    function draw() {
+      //Create the ground
+      rect(0, 319, 399, 80);
+
+      //Create the snowman body
+      ellipse(200, 300, 150, 150);
+      ellipse(200, 200, 100, 100);
+      ellipse(200, 120, 75, 75);
+
+      //Create the arms
+      line(65, 100,150, 202);
+      line(333, 100, 250, 202);
+}
